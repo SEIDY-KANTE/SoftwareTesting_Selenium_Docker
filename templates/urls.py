2d590dab9e53b1def5c742ab7bfd1c56ef54e741
Seidy Kante <seidykante21@gmail.com>
2024-05-04 22:48:02 +0300

    Project Initialization

---
+++ templates/urls.py
@@ -0,0 +1,52 @@
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("index.html", views.index, name="index"),
+    path("blog.html", views.blog, name="blog"),
+    path("blog-single-post.html", views.blog_single_post, name="blog single post"),
+    path("home-modern.html", views.home_modern, name="home modern"),
+    path("home-classic.html", views.home_classic, name="home classic"),
+    path("about-us.html", views.about, name="about us"),
+    path("404.html", views.error, name="404"),
+    path("awards.html", views.awards, name="awards"),
+    path("careers.html", views.careers, name="careers"),
+    path(
+        "case-studies-classic.html",
+        views.case_studies_classic,
+        name="case-studies-classic",
+    ),
+    path("case-studies-grid.html", views.case_studies_grid, name="case-studies-grid"),
+    path(
+        "case-studies-modern.html",
+        views.case_studies_modern,
+        name="case-studies-modern",
+    ),
+    path(
+        "case-studies-single.html",
+        views.case_studies_single,
+        name="case-studies-single",
+    ),
+    path("coming-soon.html", views.coming_soon, name="coming-soon"),
+    path("contact-us.html", views.contact, name="contact-us"),
+    path("faqs.html", views.faqs, name="faqs"),
+    path(
+        "industries-single-industry.html",
+        views.industries_single_industry,
+        name="industries-single-industry",
+    ),
+    path("industries", views.industries, name="industries"),
+    path(
+        "it-solutions-single.html",
+        views.it_solutions_single,
+        name="it-solutions-single",
+    ),
+    path("it-solutions.html", views.it_solutions, name="it-solutions"),
+    path("leadership-team.html", views.leadership_team, name="leadership-team"),
+    path("pricing.html", views.pricing, name="pricing"),
+    path("request-quote.html", views.request_quote, name="request-quote"),
+    path("search.html", views.search, name="search"),
+    path("why-us.html", views.why_choose_us, name=""),
+]
---
+++ templates/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def blog(request):
+    return render(request, "blog.html")
+
+
+def blog_single_post(request):
+    return render(request, "blog-single-post.html")
+
+
+def home_modern(request):
+    return render(request, "home-modern.html")
+
+
+def home_classic(request):
+    return render(request, "home-classic.html")
+
+
+def about(request):
+    return render(request, "about-us.html")
+
+
+def error(request):
+    return render(request, "404.html")
+
+
+def awards(request):
+    return render(request, "awards.html")
+
+
+def careers(request):
+    return render(request, "careers.html")
+
+
+def case_studies_classic(request):
+    return render(request, "case-studies-classic.html")
+
+
+def case_studies_grid(request):
+    return render(request, "case-studies-grid.html")
+
+
+def case_studies_modern(request):
+    return render(request, "case-studies-modern.html")
+
+
+def case_studies_single(request):
+    return render(request, "case-studies-single.html")
+
+
+def coming_soon(request):
+    return render(request, "coming-soon.html")
+
+
+def contact(request):
+    return render(request, "contact-us.html")
+
+
+def faqs(request):
+    return render(request, "faqs.html")
+
+
+def industries_single_industry(request):
+    return render(request, "industries-single-industry.html")
+
+
+def industries(request):
+    return render(request, "industries.html")
+
+
+def it_solutions_single(request):
+    return render(request, "it-solutions-single.html")
+
+
+def it_solutions(request):
+    return render(request, "it-solutions.html")
+
+
+def leadership_team(request):
+    return render(request, "leadership-team.html")
+
+
+def pricing(request):
+    return render(request, "pricing.html")
+
+
+def request_quote(request):
+    return render(request, "request-quote.html")
+
+
+def search(request):
+    return render(request, "search.html")
+
+
+def why_choose_us(request):
+    return render(request, "why-us.html")
